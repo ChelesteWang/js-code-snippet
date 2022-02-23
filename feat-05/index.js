119cda5f3f34fc01d27327bf3acde64c14d9716c
ChelesteWang <1581446178@qq.com>
2022-02-23 23:18:44 +0800

    1 file modified

--- feat-05/index.js
+++ feat-05/index.js
@@ -2,14 +2,21 @@ import promisify from './promisify';
 
 export default function main() {
   function test(res, cb) {
-    cb(res);
+    cb(null, res);
   }
 
-  test('callback', (res) => {
+  test('callback', (err, res) => {
+    if (err) {
+      console.error(err);
+    }
     console.log('hello', res);
   });
 
   const promisifyTest = promisify(test);
 
-  promisifyTest('promisify').then((res) => console.log('hello', res));
+  promisifyTest('promisify')
+    .then((res) => console.log('hello', res))
+    .catch((err) => {
+      console.error(err);
+    });
 }
