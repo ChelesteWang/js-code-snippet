a0ed8d8fdaa489bb43c095bd10c090cefc85c4cc
ChelesteWang <1581446178@qq.com>
2022-02-23 22:35:59 +0800

    1 file added

---
+++ feat-05/promisify.js
@@ -0,0 +1,19 @@
+export default function promisify(fn) {
+  return function (...args) {
+    let hasCb = args.some((v) => typeof v === 'function');
+    if (hasCb) {
+      fn(...args);
+    } else {
+      return new Promise(() => {
+        fn(...args, cb);
+        function cb(err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        }
+      });
+    }
+  };
+}
